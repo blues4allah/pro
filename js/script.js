c9bc078a6bb7860fc743a3ac35026c6e12d27768
Eric <ericmackjohnson@gmail.com>
2017-05-08 15:06:12 -0700

    responsive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-function toggleOverlay(){
-	var overlay = document.getElementById('overlay');
-	var specialBox = document.getElementById('specialBox');
-	overlay.style.opacity = .8;
-	if(overlay.style.display == "block"){
-		overlay.style.display = "none";
-		specialBox.style.display = "none";
-	} else {
-		overlay.style.display = "block";
-		specialBox.style.display = "block";
-	}
-}
-
-// nice
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 800);
+    return false;
+});
